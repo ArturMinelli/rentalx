8f97460c4c556b54c822bc6a40ae0317104a886c
Artur Minelli Peixoto <arturminelli@gmail.com>
2022-11-16 17:46:30 -0300

    Categories - Validating creation of category

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -7,8 +7,12 @@ const categoriesRepository = new CategoriesRepository()
 categoriesRoutes.post('/', (request, response) => {
   const { name, description } = request.body
 
-  categoriesRepository.create({name, description})
+  const categoryAlreadyExists = categoriesRepository.findByName(name)
+  if(categoryAlreadyExists) {
+    response.status(409).json({ message: "Category already exists!" })
+  }
 
+  categoriesRepository.create({name, description})
   response.status(201).send()
 })
 
