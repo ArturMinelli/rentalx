880883e5c94a014a127122df0a7c8d8780bdee78
Artur Minelli Peixoto <arturminelli@gmail.com>
2022-11-17 09:16:43 -0300

    Categories - Creating CreateCategoryService

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express'
 import { CategoriesRepository } from '../repositories/CategoriesRepository'
+import { CreateCategoryService } from '../services/CreateCategoryService'
 
 export const categoriesRoutes = Router()
 const categoriesRepository = new CategoriesRepository()
@@ -7,12 +8,9 @@ const categoriesRepository = new CategoriesRepository()
 categoriesRoutes.post('/', (request, response) => {
   const { name, description } = request.body
 
-  const categoryAlreadyExists = categoriesRepository.findByName(name)
-  if(categoryAlreadyExists) {
-    response.status(409).json({ message: "Category already exists!" })
-  }
+  const createCategoryService = new CreateCategoryService(categoriesRepository)
+  createCategoryService.execute({ name, description })
 
-  categoriesRepository.create({name, description})
   response.status(201).send()
 })
 
