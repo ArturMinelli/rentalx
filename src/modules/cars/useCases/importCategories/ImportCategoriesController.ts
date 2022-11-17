fd32d6bcb1672a1e3183d71bf42068d51c1dd581
Artur Minelli Peixoto <arturminelli@gmail.com>
2022-11-17 14:42:41 -0300

    Categories - Reading csv file

--- src/modules/cars/useCases/importCategories/ImportCategoriesController.ts
+++ src/modules/cars/useCases/importCategories/ImportCategoriesController.ts
@@ -9,7 +9,7 @@ export class ImportCategoriesController {
   handle(request: Request, response: Response): Response {
     const { file } = request
 
-    console.log(file)
+    this.importCategoriesUseCase.execute(file)
 
     return response.send()
   }
--- src/modules/cars/useCases/importCategories/ImportCategoriesUseCase.ts
+++ src/modules/cars/useCases/importCategories/ImportCategoriesUseCase.ts
@@ -1,11 +1,21 @@
 import { CategoriesRepository } from "../../repositories/implementations/CategoriesRepository";
+import fs from 'fs'
+import { parse } from 'csv-parse'
 
 export class ImportCategoriesUseCase {
   constructor(private categoriesRepository: CategoriesRepository) {
     this.categoriesRepository = categoriesRepository
   }
 
-  execute(filePath: string) {
+  execute(file: Express.Multer.File): void {
 
+    const stream = fs.createReadStream(file.path)
+    const parseFile = parse()
+
+    stream.pipe(parseFile)
+
+    parseFile.on('data', async (line) => {
+      console.log(line)
+    })
   }
 }
