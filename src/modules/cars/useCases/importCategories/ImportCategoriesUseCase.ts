c4efcafb7a032705ea85b4e1972fb9c108574ecc
Artur Minelli Peixoto <arturminelli@gmail.com>
2022-11-17 15:19:29 -0300

    Categories - Creating loadCategories function to parse the csv file

--- src/modules/cars/useCases/importCategories/ImportCategoriesUseCase.ts
+++ src/modules/cars/useCases/importCategories/ImportCategoriesUseCase.ts
@@ -1,21 +1,45 @@
-import { CategoriesRepository } from "../../repositories/implementations/CategoriesRepository";
 import fs from 'fs'
 import { parse } from 'csv-parse'
+import { ICategoriesRepository } from '../../repositories/ICategoriesRepository'
+
+interface IImportCategories {
+  name: string;
+  description: string;
+}
 
 export class ImportCategoriesUseCase {
-  constructor(private categoriesRepository: CategoriesRepository) {
+  constructor(private categoriesRepository: ICategoriesRepository) {
     this.categoriesRepository = categoriesRepository
   }
 
-  execute(file: Express.Multer.File): void {
+  loadCategories(file: Express.Multer.File): Promise<IImportCategories[]> {
+    return new Promise((resolve, reject) => {
+      const stream = fs.createReadStream(file.path)
+      const parseFile = parse()
+      const loadedCategories: IImportCategories[] = []
 
-    const stream = fs.createReadStream(file.path)
-    const parseFile = parse()
+      stream.pipe(parseFile)
 
-    stream.pipe(parseFile)
+      parseFile.on('data', async ([name, description]) => {
 
-    parseFile.on('data', async (line) => {
-      console.log(line)
+        const categoryAlreadyExists = this.categoriesRepository.findByName(name)
+        if(categoryAlreadyExists) {
+          throw new Error(`Category ${name} already exists`)
+        }
+
+        loadedCategories.push({ name, description })
+      })
+      .on('end', () => {
+        resolve(loadedCategories)
+      })
+      .on('error', (error) => {
+        reject(error)
+      })
     })
+  }
+
+  async execute(file: Express.Multer.File): Promise<void> {
+    const categories = await this.loadCategories(file)
+    console.log(categories)
   }
 }
