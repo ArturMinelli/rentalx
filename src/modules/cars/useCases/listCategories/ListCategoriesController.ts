04d78dbd782f25ca5be08f5cd34b8122064fbf50
Artur Minelli Peixoto <arturminelli@gmail.com>
2022-11-21 14:09:54 -0300

    Fix - Making container update automatically whenever file is saved

--- src/modules/cars/useCases/listCategories/ListCategoriesController.ts
+++ src/modules/cars/useCases/listCategories/ListCategoriesController.ts
@@ -8,7 +8,6 @@ export class ListCategoriesController {
 
   handle(request: Request, response: Response): Response {
     const categories = this.listCategoriesUseCase.execute()
-
     return response.status(200).json(categories)
   }
 }
