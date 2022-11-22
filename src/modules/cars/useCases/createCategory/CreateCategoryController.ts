377ae19d219d566fca891dba4a524da843f634b9
Artur Minelli Peixoto <arturminelli@gmail.com>
2022-11-22 10:17:29 -0300

    Using tsyringe to make dependencies injection in the CreateCategoryUseCase class

--- src/modules/cars/useCases/createCategory/CreateCategoryController.ts
+++ src/modules/cars/useCases/createCategory/CreateCategoryController.ts
@@ -1,16 +1,15 @@
 import { Request, Response } from 'express'
+import { container } from 'tsyringe'
 import { CreateCategoryUseCase } from './CreateCategoryUseCase'
 
 export class CreateCategoryController {
-  constructor(private createCategoryUseCase: CreateCategoryUseCase) {
-    this.createCategoryUseCase = createCategoryUseCase
-  }
 
   async handle(request: Request, response: Response): Promise<Response> {
     const { name, description } = request.body
 
     try {
-      await this.createCategoryUseCase.execute({ name, description })
+      const createCategoryUseCase = container.resolve(CreateCategoryUseCase)
+      await createCategoryUseCase.execute({ name, description })
     }
     catch (error) {
       return response.status(400).json({ error: error.message })
